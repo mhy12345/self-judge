725b4cd155c32a48c54cc83ec6c2431b1f851edb
mhy12345 <maohanyang789@163.com>
2015-01-10 00:45:12 +0800

    .cfg文件可选择位置，并作为当前文件夹。 文件内部分离类定义与方法。 更新大部分重要变量名。

--- judge.cpp
+++ judge.cpp
@@ -20,6 +20,8 @@ using namespace std;
 #define RS_RE 3
 #define RS_MLE 4
 #define RS_WA 5
+#define ROOT_PATH "/home/toby/Program"
+//PrintLog{{{
 inline void PrintLog(const char *x,int y,int z)
 {
 		FILE *Log=fopen("log.txt","a");
@@ -41,7 +43,6 @@ inline void PrintLog(const char *x)
 		fclose(Log);
 		//				printf("%s",x);
 }
-
 
 void Print(const char *cc)
 {
@@ -66,127 +67,162 @@ void Print(const char *cc)
 				printf("\033[36m%s",cc);
 		}else
 				printf("%s\n",cc);
+		printf("\033[0m");
 }
+//}}}
+
 class Task;
 class TestCase
 {
-		public:
-				Task* task;
-				int id;
-				int TimeLimit,MemoryLimit;
+		private:
+				const Task* task;
+				int TimeLimit;
+				int MemoryLimit;
+				int Id;
 				int Score;
 				int Time,Memory;
 				int Status;
-				void Print_Status()
-				{
-						switch (Status)
-						{
-								case RS_AC : ::Print("Accept               ");break;
-								case RS_TLE: ::Print("Time Limit Exceed    ");break;
-								case RS_WA : ::Print("Wrong Answer         ");break;
-								case RS_MLE: ::Print("Memory Limit Exceed  ");break;
-								case RS_RE : ::Print("Runtime Error        ");break;
-								default:     ::Print("Unknown Result       ");break;
-						}
-						printf("\033[0m% 6ds   % 6dkb\n",Time,Memory);
-				}
-				void SetTimeLimit(int TimeLimit)
-				{
-						this->TimeLimit=TimeLimit;
-				}
-				void SetMemoryLimit(int MemoryLimit)
-				{
-						this->MemoryLimit=MemoryLimit;
-				}
+		public:
+				TestCase(const Task* task,int TimeLimit,int MemoryLimit,int Id);
 				int Run();
-
+				void Print_Status();
+				void SetTimeLimit(int TimeLimit);
+				void SetMemoryLimit(int MemoryLimit);
+				void SetStatus(int Status);
+				int GetStatus();
+				const Task* GetTask();
+				void SetTask(const Task* task);
 };
 class Task
 {
 		private:
-				char din[STRLEN],dout[STRLEN];
-				int lrange,rrange;
-				char sname[STRLEN];//Source File Name
-				char exname[STRLEN];
-				char pin[STRLEN],pout[STRLEN];//Input
-				int tlim[TOTCASE],mlim[TOTCASE];
-				TestCase *testc[TOTCASE];
-				int totcase;
+				static const int max_case=TOTCASE;
+				static const int max_length=STRLEN;
+				char DataIn[max_length],DataOut[max_length];
+				int LeftRange,RightRange;
+				char SourceName[max_length];//Source File Name
+				char ExecuteName[max_length];
+				char FileIn[max_length],FileOut[max_length];//Input
+				TestCase *testc[max_case];
+				int TotalCase;
 		public:
-				friend int TestCase::Run();
-				~Task()
-				{
-						for (int i=0;i<totcase;i++)
-								delete testc[i];
-				}
-				bool Compile()
-				{
-						char cc[STRLEN];
-						sprintf(cc,"g++ %s -o ./tmp/%s >Compile.log",sname,exname);
-						if (system(cc))return true;
-						return false;
-				}
-				int Judge()
-				{
-						char in[STRLEN],out[STRLEN],cc[STRLEN];
-						for (int i=lrange;i<=rrange;i++)
-						{
-								//printf("\033[46mCase %d:",i);printf("\033[0m\n");
-								PrintLog("Case %d:\n",i);
-								sprintf(in,din,i);
-								sprintf(out,dout,i);
-								sprintf(cc,"cp %s ./tmp/%s",in,pin);
-								system(cc);
-								testc[i]->Run();
-								if (!testc[i]->Status)
-								{
-										sprintf(cc,"diff %s ./tmp/%s -w >res%d_diff.log",out,pout,i);
-										if (system(cc))
-										{
-												//Print("Wrong Answer\n");
-												testc[i]->Status=RS_WA;
-										}else
-										{
-												//Print("Accept\n");
-												testc[i]->Status=RS_AC;
-										}
-								}
-								testc[i]->Print_Status();
-						}
-						return 0;
-				}
-				void Init(const char *cfg_name="log.txt")
-				{
-						FILE *Log=fopen(cfg_name,"w");
-						fclose(Log);
-						fprintf(Log,"Judging Time:--\n");
-						freopen("/dev/null","w",stderr);
-						FILE *gs=fopen("judge.cfg","r");
-						fscanf(gs,"%s\n",exname);//Project Name
-						fscanf(gs,"%s\n",din);//Data Input
-						fscanf(gs,"%s\n",dout);//Data Output
-						fscanf(gs,"%d %d\n",&lrange,&rrange);//Index Range
-						fscanf(gs,"%s\n",sname);//Source File Name
-						fscanf(gs,"%s\n",pin);//Input File Name
-						fscanf(gs,"%s\n",pout);//Output File Name
-						int TimeLimit,MemoryLimit;
-						fscanf(gs,"%d%d",&TimeLimit,&MemoryLimit);
-						totcase=rrange-lrange+1;
-						for (int i=lrange;i<=rrange;i++)
-						{
-								testc[i-lrange]=new TestCase;
-								testc[i-lrange]->SetTimeLimit(TimeLimit);
-								testc[i-lrange]->SetMemoryLimit(MemoryLimit);
-								testc[i-lrange]->task=this;
-								testc[i-lrange]->id=i;
-						}
-				}
+				~Task();
+				const char *GetExecuteName()const;
+				bool Compile()const;
+				int Judge();
+				void Init(const char *cfg_name);
 
-}T;
-void call()
+};
+
+Task::~Task()
 {
+		for (int i=0;i<TotalCase;i++)delete testc[i];
+}
+const char *Task::GetExecuteName()const
+{
+		return ExecuteName;
+}
+bool Task::Compile()const
+{
+		char cc[STRLEN];
+		system("mkdir ./tmp");
+		sprintf(cc,"g++ %s -o ./tmp/%s >Compile.log",SourceName,ExecuteName);
+		if (system(cc))return true;
+		return false;
+}
+int Task::Judge()
+{
+		char in[STRLEN],out[STRLEN],cc[STRLEN],LogDir[STRLEN];
+		sprintf(cc,"mkdir %s_difflog",ExecuteName);
+		sprintf(LogDir,"%s_difflog",ExecuteName);
+		system(cc);
+		for (int i=LeftRange;i<=RightRange;i++)
+		{
+				PrintLog("Case %d:\n",i);
+				sprintf(in,DataIn,i);
+				sprintf(out,DataOut,i);
+				sprintf(cc,"cp %s ./tmp/%s",in,FileIn);
+				system(cc);
+				testc[i]->Run();
+				if (!testc[i]->GetStatus())
+				{
+						sprintf(cc,"diff %s ./tmp/%s -w >%s/DiffLog%d.txt",out,FileOut,LogDir,i);
+						if (system(cc))
+						{
+								//Print("Wrong Answer\n");
+								testc[i]->SetStatus(RS_WA);
+						}else
+						{
+								//Print("Accept\n");
+								sprintf(cc,"echo Accept >%s/DiffLog%d.txt",LogDir,i);
+								system(cc);
+								testc[i]->SetStatus(RS_AC);
+						}
+				}
+				testc[i]->Print_Status();
+		}
+		return 0;
+}
+void Task::Init(const char *cfg_name="log.txt")
+{
+		FILE *Log=fopen("log.txt","w");
+		fclose(Log);
+		fprintf(Log,"Judging Time:--\n");
+		freopen("/dev/null","w",stderr);
+		FILE *gs=fopen(cfg_name,"r");
+		fscanf(gs,"%s\n",ExecuteName);//Project Name
+		fscanf(gs,"%s\n",DataIn);//Data Input
+		fscanf(gs,"%s\n",DataOut);//Data Output
+		fscanf(gs,"%d %d\n",&LeftRange,&RightRange);//Index Range
+		fscanf(gs,"%s\n",SourceName);//Source File Name
+		fscanf(gs,"%s\n",FileIn);//Input File Name
+		fscanf(gs,"%s\n",FileOut);//Output File Name
+		int TimeLimit,MemoryLimit;
+		fscanf(gs,"%d%d",&TimeLimit,&MemoryLimit);
+		TotalCase=RightRange-LeftRange+1;
+		for (int i=LeftRange;i<=RightRange;i++)
+		{
+				testc[i-LeftRange]=new TestCase(this,TimeLimit,MemoryLimit,i);
+		}
 }
 
+TestCase::TestCase(const Task* task,int TimeLimit,int MemoryLimit,int Id):
+		task(task),TimeLimit(TimeLimit),MemoryLimit(MemoryLimit),Id(Id)
+{
+};
+void TestCase::Print_Status()
+{
+		switch (Status)
+		{
+				case RS_AC : ::Print("Accept               ");break;
+				case RS_TLE: ::Print("Time Limit Exceed    ");break;
+				case RS_WA : ::Print("Wrong Answer         ");break;
+				case RS_MLE: ::Print("Memory Limit Exceed  ");break;
+				case RS_RE : ::Print("Runtime Error        ");break;
+				default:     ::Print("Unknown Result       ");break;
+		}
+		printf("\033[0m% 6ds   % 6dkb\n",Time,Memory);
+}
+void TestCase::SetTimeLimit(int TimeLimit)
+{
+		this->TimeLimit=TimeLimit;
+}
+void TestCase::SetMemoryLimit(int MemoryLimit)
+{
+		this->MemoryLimit=MemoryLimit;
+}
+void TestCase::SetStatus(int Status)
+{
+		this->Status=Status;
+}
+int TestCase::GetStatus()
+{
+		return Status;
+}
+const Task* TestCase::GetTask()
+{
+		return task;
+}
 int TestCase::Run()
 {
 		char cc[STRLEN];
@@ -205,10 +241,10 @@ int TestCase::Run()
 				getrlimit(RLIMIT_AS,&rm_as_old);
 				rm_as_new.rlim_cur=(MemoryLimit+1)*1024*1024;
 				rm_as_old.rlim_max=(MemoryLimit+1)*1024*1024;
-				PrintLog("Set Memory Limit %d\n",rm_as_new.rlim_cur);
+				PrintLog("Set Memory Limit %d\n",(int)rm_as_new.rlim_cur);
 				setrlimit(RLIMIT_AS,&rm_as_new);
 
-				sprintf(cc,"./%s </dev/null >/dev/null \n",task->exname);
+				sprintf(cc,"./%s </dev/null >/dev/null \n",task->GetExecuteName());
 				PrintLog(cc);
 				getcwd(path_old,440);
 				strcpy(path_new,path_old);
@@ -273,9 +309,60 @@ int TestCase::Run()
 		}
 		return 0;
 }
+
+Task T;
 int main(int argc,const char* args[])
 {
-		T.Init();
+		char cfg_path[STRLEN];
+		char str[STRLEN];
+		printf("\033[0m\n");
+		strcpy(cfg_path,ROOT_PATH);
+		chdir(cfg_path);
+		if (args[1])
+		{
+				strcpy(cfg_path,args[1]);
+		}else
+		{
+				printf("\033[33mConfigure Files:\033[0m\n");
+				while (fgets(str,STRLEN,stdin))
+				{
+						if (strstr(str,"ls")==str)
+						{
+								system("ls --color=always");
+						}else if (strstr(str,"cd ..")==str)
+						{
+								getcwd(cfg_path,STRLEN);
+								int len=(int)strlen(cfg_path);
+								for (int i=len-1;i>=0;i--)
+										if (cfg_path[i]=='/')
+										{
+												cfg_path[i]='\0';
+												break;
+										}
+								chdir(cfg_path);
+								//printf(str);
+						}else if (strstr(str,"cd ")==str)
+						{
+								system(str);
+								str[strlen(str)-1]='\0';
+								strcat(cfg_path,"/");
+								strcat(cfg_path,str+3);
+								chdir(cfg_path);
+						}else if (strstr(str,"vim ")==str)
+						{
+								system(str);
+						}else
+						{
+								str[strlen(str)-1]='\0';
+								getcwd(cfg_path,STRLEN);
+								strcat(cfg_path,"/");
+								strcat(cfg_path,str);
+								break;
+						}
+						printf("%s\n",cfg_path);
+				}
+		}
+		T.Init(cfg_path);
 		if (T.Compile())
 		{
 				Print("Compile Error!\n");
